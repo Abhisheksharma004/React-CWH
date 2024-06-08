38de510ccc61251d98b151146c734e079550df62
Abhisheksharma004 <abhishekkumarranjan965@gmail.com>
2024-06-08 18:22:36 +0530

    bg color change project some syntax change

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -10,35 +10,50 @@ function App() {
     >
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-xl">
-          <button onClick={()=>{setColor("red")}}
+          <button
+            onClick={() => {
+              setColor("red");
+            }}
             className=" outline-none px-4 py-1 rounded-full text-white shadow-lg"
             style={{ backgroundColor: "red" }}
           >
             Red
           </button>
 
-          <button onClick={()=>{setColor("green")}}
+          <button
+            onClick={() => {
+              setColor("green");
+            }}
             className=" outline-none px-4 py-1 rounded-full text-white shadow-lg"
             style={{ backgroundColor: "green" }}
           >
             Green
           </button>
 
-          <button onClick={()=>{setColor("blue")}}
+          <button
+            onClick={() => {
+              setColor("blue");
+            }}
             className=" outline-none px-4 py-1 rounded-full text-white shadow-lg"
             style={{ backgroundColor: "blue" }}
           >
             Blue
           </button>
 
-          <button onClick={()=>{setColor("yellow")}}
+          <button
+            onClick={() => {
+              setColor("yellow");
+            }}
             className=" outline-none px-4 py-1 rounded-full text-white shadow-lg"
             style={{ backgroundColor: "yellow" }}
           >
             Yellow
           </button>
 
-          <button onClick={()=>{setColor("black")}}
+          <button
+            onClick={() => {
+              setColor("black");
+            }}
             className=" outline-none px-4 py-1 rounded-full text-white shadow-lg"
             style={{ backgroundColor: "black" }}
           >
