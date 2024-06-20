5e1848f948b9b0679486bad2afa3154565b5c20c
Abhisheksharma004 <abhishekkumarranjan965@gmail.com>
2024-06-20 09:36:47 +0530

    some changes

---
+++ my-jit-project/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Navbar(){
+    return(
+        <nav>
+            <div><h1>Logo</h1></div>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Gallery</a></li>   
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    )
+}
