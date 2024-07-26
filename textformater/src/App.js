4eb28d3875ff838ee119923fae9aa1e80b09c39c
Abhisheksharma004 <abhishekkumarranjan965@gmail.com>
2024-07-26 23:58:39 +0530

    Modifi text form or convert uppar casr or lowar case

--- textformater/src/App.js
+++ textformater/src/App.js
@@ -6,7 +6,9 @@ function App(prop) {
   return (
     <>
         <Navbar title="Text Formater Abhishek" home="Home" about="About" fo="F&O" con="Contact" hep="Help?" />
+        <div className="container">
         <TextBox />
+        </div>
     </>
   );
 }
--- textformater/src/components/TextBox.js
+++ textformater/src/components/TextBox.js
@@ -1,13 +1,33 @@
-import React from 'react'
+import { useState } from "react"
 
 function TextBox() {
+
+    const handleOnClick = () => {
+        console.log("Uppar case clicked")
+        let newText = text.toUpperCase()
+        setText(newText)
+    }
+
+    const handleOnClickLow = () => {
+        let newText = text.toLowerCase()
+        setText(newText)
+    }
+
+    const hendleOnChange = (event) => {
+        setText(event.target.value)
+    }
+
+
+    const [text, setText] = useState("Enter Text Here...");
+
     return (
         <div className="container mb-5">
             <h1>Text Formater</h1>
             <div className="mb-3">
-                <textarea className="form-control" rows="8" placeholder='Enter Text Here'></textarea>
+                <textarea className="form-control" rows="8" value={text} onChange={hendleOnChange} placeholder='Enter Text Here'></textarea>
             </div>
-            <button type="button" className="btn btn-outline-success">Success</button>
+            <button type="button" className="btn btn-outline-success" onClick={handleOnClick}>Conver to Uppar Case</button>
+            <button type="button" className="btn btn-outline-success" onClick={handleOnClickLow}>Conver to Lower Case</button>
         </div>
     )
 }
