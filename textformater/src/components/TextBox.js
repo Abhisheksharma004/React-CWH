6107be0d1c7ed98b616f6a498534c464c37ca808
Abhisheksharma004 <abhishekkumarranjan965@gmail.com>
2024-07-28 12:32:47 +0530

    add to word or text counter and preview

--- textformater/src/components/TextBox.js
+++ textformater/src/components/TextBox.js
@@ -21,14 +21,23 @@ function TextBox() {
     const [text, setText] = useState("Enter Text Here...");
 
     return (
-        <div className="container mb-5">
+        <>
+        <div className="container mb-3">
             <h1>Text Formater</h1>
             <div className="mb-3">
                 <textarea className="form-control" rows="8" value={text} onChange={hendleOnChange} placeholder='Enter Text Here'></textarea>
             </div>
-            <button type="button" className="btn btn-outline-success" onClick={handleOnClick}>Conver to Uppar Case</button>
-            <button type="button" className="btn btn-outline-success" onClick={handleOnClickLow}>Conver to Lower Case</button>
+            <button type="button" className="btn btn-outline-success mx-1" onClick={handleOnClick}>Conver to Uppar Case</button>
+            <button type="button" className="btn btn-outline-success mx-1" onClick={handleOnClickLow}>Conver to Lower Case</button>
         </div>
+        <div className="container my-3">
+            <h2>Text Summary</h2>
+            <p>Count Word : {text.split(" ").length} and character : {text.length} </p>
+            <p>{0.08 * text.split(" ").length} Minutes to read</p>
+            <h2>Preview</h2>
+            <p>{text}</p>
+        </div>
+        </>
     )
 }
 
