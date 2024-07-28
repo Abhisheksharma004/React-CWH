9a7053f5ddaabf2afc065307fd04f39ed80ec0d5
Abhisheksharma004 <abhishekkumarranjan965@gmail.com>
2024-07-28 16:53:10 +0530

    add copy button

--- textformater/src/components/TextBox.js
+++ textformater/src/components/TextBox.js
@@ -3,7 +3,6 @@ import { useState } from "react"
 function TextBox() {
 
     const handleOnClick = () => {
-        console.log("Uppar case clicked")
         let newText = text.toUpperCase()
         setText(newText)
     }
@@ -12,23 +11,36 @@ function TextBox() {
         let newText = text.toLowerCase()
         setText(newText)
     }
+    const copyText = () => {
+        var text = document.getElementById("myBox");
+        text.select()
+        navigator.clipboard.writeText(text.value);
+    }
+
+    const ClearText = () => {
+        let newText = ""
+        setText(newText)
+    }
+
 
     const hendleOnChange = (event) => {
         setText(event.target.value)
     }
 
 
-    const [text, setText] = useState("Enter Text Here...");
+    const [text, setText] = useState("");
 
     return (
         <>
         <div className="container mb-3">
             <h1>Text Formater</h1>
             <div className="mb-3">
-                <textarea className="form-control" rows="8" value={text} onChange={hendleOnChange} placeholder='Enter Text Here'></textarea>
+                <textarea id="myBox" className="form-control" rows="8" value={text} onChange={hendleOnChange} placeholder='Enter Text Here'></textarea>
             </div>
             <button type="button" className="btn btn-outline-success mx-1" onClick={handleOnClick}>Conver to Uppar Case</button>
             <button type="button" className="btn btn-outline-success mx-1" onClick={handleOnClickLow}>Conver to Lower Case</button>
+            <button type="button" className="btn btn-outline-success mx-1" onClick={copyText}>Copy Text</button>
+            <button type="button" className="btn btn-outline-danger mx-1" onClick={ClearText}>Clear Text</button>
         </div>
         <div className="container my-3">
             <h2>Text Summary</h2>
